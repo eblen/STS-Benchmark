d738f23944cbe160d3318a0ac248e2ea2d05e175
John Eblen <jeblen@acm.org>
2016-04-13 08:36:21 -0700

    Add licensing information

--- syncbench.cpp
+++ syncbench.cpp
@@ -16,6 +16,9 @@
 *      This version copyright (c) The University of Edinburgh, 2015.        *
 *                                                                           *
 *                                                                           *
+*      modified by John Eblen (jeblen@acm.org) for benchmarking STS         *
+*                                                                           *
+*                                                                           *
 *  Licensed under the Apache License, Version 2.0 (the "License");          *
 *  you may not use this file except in compliance with the License.         *
 *  You may obtain a copy of the License at                                  *
